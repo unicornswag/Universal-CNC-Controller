84667b50eec51c73e68d258c6fd5a144e71c7c4d
unicornswag <14pavicada@14pavicada.cameron.k12.wi.us>
2014-03-26 08:40:31 -0500

    added README
    added README

--- help.sh
+++ help.sh
@@ -15,11 +15,18 @@ echo " "
 echo "This set of shell scripts acts as a wrapper to stream.py python"
 echo "script provided with these scripts and available at: "
 echo "https://github.com/grbl/grbl/blob/master/script/stream.py"
-echo " "
+echo "Note that stream.py requires that the py_serial module be installed."
+echo "These scripts provide a more interactive interface to stream files,"
+echo "jog your machine, and enter manual gcode commmands. This program"
+echo "should be compatable with any ATMel AVR CNC firmware, including "
+echo "grbl, Teacup, Repetier, Marlin, etc. By default, you will be "
+echo "connected to the device located at /dev/ttyACM0. However, this can"
+echo "be changed to suit your needs in start.sh."
 
-echo "You can start the program by executing any of the shell scripts"
-echo "provided in this folder. From there, you can enter the letter"
-echo "command corresponding to the desired function." 
+echo "You can start the program by executing the shell script, 'start.sh'"
+echo "provided in this directory. From there, you can enter the letter"
+echo "command corresponding to the desired function listed at the top"
+echo "of the window. "
 if
 
 read -n 1 EXIT
